1f7f734219a6a727a65f4d0c503ae119bf13bbb6
sandeepan.sportiz <sandeepan.sportiz@gmail.com>
2024-04-16 12:03:02 +0530

    Implemented doFilterInternal method

--- src/main/java/com/secu/auth/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/secu/auth/configuration/JwtAuthenticationFilter.java
@@ -4,6 +4,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,15 +18,25 @@ import java.io.IOException;
 import java.net.http.HttpHeaders;
 import java.util.Optional;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         Optional<String> tokenOptional = getTokenFromRequest(request);
         tokenOptional.ifPresent(jwtToken -> {
-            if(JwtUtils.validateToken(jwtToken)){
-                JwtUtils.getUsernameFromToken(jwtToken);
+            if (JwtUtils.validateToken(jwtToken)) {
+                String username = JwtUtils.getUsernameFromToken(jwtToken);
+                var userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         });
+        filterChain.doFilter(request, response);
     }
 
     private Optional<String> getTokenFromRequest(HttpServletRequest request) {
